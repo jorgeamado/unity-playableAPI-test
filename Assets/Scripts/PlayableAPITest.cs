cd951da78d2d2360b66bff491694979980c4c8d9
georgrel <george.lemeshko@gmail.com>
2017-04-24 14:35:29 +0300

    Add char for playable API test

---
+++ Assets/Scripts/PlayableAPITest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Director;
+
+public class PlayableAPITest : MonoBehaviour
+{
+    public Animator PlayerAnimatorController;
+    public Animator WeaponAnimatorController;
+    public float weight;
+
+    private PlayableGraph playableGraph;
+    private PlayableHandle mixer;
+
+    void Start()
+    {
+        mixer = AnimationPlayableUtilities.PlayMixer(GetComponent<Animator>(), 2, out playableGraph);
+        playableGraph.Connect(playableGraph.CreateAnimatorControllerPlayable(PlayerAnimatorController.runtimeAnimatorController), 0, mixer, 0);
+        playableGraph.Connect(playableGraph.CreateAnimatorControllerPlayable(WeaponAnimatorController.runtimeAnimatorController), 0, mixer, 1);
+        GraphVisualizerClient.Show(playableGraph, name);
+    }
+
+    void Update()
+    {
+        weight = Mathf.Clamp01(weight);
+        mixer.SetInputWeight(0, 1.0f - weight);
+        mixer.SetInputWeight(1, weight);
+    }
+
+    void OnDisable()
+    {
+        // Destroys all Playables and Outputs created by the graph.
+        playableGraph.Destroy();
+    }
+
+}
---
+++ Assets/Scripts/QuickAnimatorParameterSetter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class QuickAnimatorParameterSetter : MonoBehaviour
+{
+	public KeyCodeAnimatorParameter[] Keys;
+	public Animator _a;
+	// Use this for initialization
+	void Start ()
+	{
+		_a = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (_a == null)
+		{
+			_a = GetComponent<Animator>();
+			return;
+		}
+
+		foreach (var k in Keys)
+		{
+			k.ProcessInput(_a);
+		}
+	}
+}
+
+
+[System.Serializable]
+public class KeyCodeAnimatorParameter
+{
+	public KeyCode PositiveKeyCode;
+	public KeyCode NegativeKeyCode;
+	public string Parameter;
+
+    private Action PositiveAction;
+    private Action NegativeAction;
+    [SerializeField] private bool _isSetted;
+
+
+    public void ProcessInput(Animator animator)
+    {
+        if (!_isSetted)
+        {
+            SetupActions(animator);
+        }
+
+        if (Input.GetKeyUp(PositiveKeyCode))
+            PositiveAction();
+        else if (Input.GetKeyUp(NegativeKeyCode))
+            NegativeAction();
+
+    }
+
+    private void SetupActions(Animator animator)
+    {
+        var animparameter = animator.parameters.FirstOrDefault(parameter => parameter.name == Parameter);
+        if (animparameter != null)
+        {
+            if (animparameter.type == AnimatorControllerParameterType.Bool)
+                SetupBoolActions(animator);
+        }
+    }
+
+    private void SetupBoolActions(Animator animator)
+    {
+        PositiveAction = () => animator.SetBool(Parameter, true);
+        NegativeAction = () => animator.SetBool(Parameter, false);
+        _isSetted = true;
+    }
+}
